7dea716fb7389455cb040c6ea8030dd17c0b0e86
sierrabonita <workbj1212@gmail.com>
2025-09-01 12:14:40 +0900

    Refactor main page to use Chakra UI components and add a new pagination page

---
+++ src/app/pagination/page.tsx
@@ -0,0 +1,9 @@
+import { Heading, Link, Stack } from "@chakra-ui/react";
+
+export default function Page() {
+  return (
+      <Stack>
+        <Heading size={'3xl'}>Pagination</Heading>
+      </Stack>
+  );
+}
